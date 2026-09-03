2111e5e64c8a0d3cf37dcc5df4a56e03be7d19cf
Eason Yang <lime@limooo.cn>
2026-09-04 01:04:41 +0800

    fix(build): use system font stack so build doesn't fetch Google Fonts

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,11 +1,2 @@
-import { JetBrains_Mono as FontMono, Noto_Sans_SC as FontSans } from 'next/font/google';
-
-export const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-});
-
-export const fontMono = FontMono({
-  subsets: ['latin'],
-  variable: '--font-mono',
-});
+export const fontSans = { variable: '' };
+export const fontMono = { variable: '' };
